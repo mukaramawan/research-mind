96306818388e4ce7aac28975a8b93b4fd0da2475
Muhammad Mukarram Awan <141765944+mukaramawan@users.noreply.github.com>
2026-04-14 21:29:49 +0500

    agents

---
+++ agents.py
@@ -0,0 +1,67 @@
+from langchain.agents import create_agent
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_mistralai import MistralAI
+from dotenv import load_dotenv
+from tools import search_web, scrape_web
+
+load_dotenv()
+
+llm = MistralAI(model="mistral-medium-latest", temperature=0)
+
+def build_search_agent():
+    return create_agent(
+        model = llm,
+        tools=[search_web]
+    )
+
+def build_reader_agent():
+    return create_agent(
+        model = llm,
+        tools=[scrape_web]
+    )
+
+writer_prompt =  ChatPromptTemplate.from_messages([
+    ("system", "You are an expert research writer. Write clear, structured and insightful report."),
+    ("human", """Write a detail research report on the given topic.
+     Topic: {topic}
+
+     Research Gathered: {researchGathered}
+
+     Structure the report as:
+     - Introduction
+     - Key findings
+     - Conclusion
+     - Sources
+
+     Report should be in detail, factual and professional.
+     """)
+])
+
+
+writer =  writer_prompt | llm | StrOutputParser()
+
+critic_prompt = ChatPromptTemplate.from_messages([
+     ("system", "You are a sharp and constructive research critic. Be honest and specific."),
+    ("human", """Review the research report below and evaluate it strictly.
+
+Report:
+{report}
+
+Respond in this exact format:
+
+Score: X/10
+
+Strengths:
+- ...
+- ...
+
+Areas to Improve:
+- ...
+- ...
+
+One line verdict:
+..."""),
+])
+
+critic = critic_prompt | llm | StrOutputParser()
